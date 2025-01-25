3f83789efb8d1de34edca4c94ad0f784fc7c8d74
KasraRahimi <kasra.rahimi@polymtl.ca>
2025-01-25 15:30:46 -0500

    feat: weather handler added to server

--- backend/server/server.go
+++ backend/server/server.go
@@ -12,6 +12,8 @@ const AiEndpoint = baseUrl + "ai"
 func GetServerMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	// Weather Handlers here
+	mux.Handle(WeatherEndpoint, &WeatherHandler{})
 	// AI Handlers here
 	mux.Handle(SegmentEndpoint, &SegmentHandler{})
 	return mux
