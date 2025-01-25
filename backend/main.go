c18db5cf7b856ea58ca51a943839f0a62df03c9d
KasraRahimi <kasra.rahimi@polymtl.ca>
2025-01-25 12:35:02 -0500

    feat: simple main function that starts server

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,21 @@
 package main
 
 import(
+	"fitcast/server"
 	"fmt"
+	"log"
+	"net/http"
 )
 
+const port = ":8080"
+
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Starting server...")
+
+	mux := server.GetServerMux()
+	fmt.Printf("Server listening on %s\n", port)
+	err := http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
