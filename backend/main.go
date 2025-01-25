fa596e23b094cc5e23266a56336ad8cceefb7bb8
KasraRahimi <kasra.rahimi@polymtl.ca>
2025-01-25 17:39:26 -0500

    feat: loading in env variables

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fitcast/server"
 	"fmt"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 )
@@ -11,10 +12,14 @@ const port = ":8080"
 
 func main() {
 	fmt.Println("Starting server...")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	mux := server.GetServerMux()
 	fmt.Printf("Server listening on %s\n", port)
-	err := http.ListenAndServe(port, mux)
+	err = http.ListenAndServe(port, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
