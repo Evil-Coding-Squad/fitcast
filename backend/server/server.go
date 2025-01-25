9d757f097f196109f86878f07d3e20be5303e5ce
KasraRahimi <kasra.rahimi@polymtl.ca>
2025-01-25 14:03:11 -0500

    feat: set up some endpoints

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,8 +1,14 @@
 package server
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
+
+const baseUrl = "/api/"
+
+const WeatherEndpoint = baseUrl + "weather/"
 
 func GetServerMux() *http.ServeMux {
 	mux := http.NewServeMux()
