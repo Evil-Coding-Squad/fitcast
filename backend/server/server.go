67d7cb3bb33184de25dde154f8668befb4125c23
KasraRahimi <kasra.rahimi@polymtl.ca>
2025-01-25 23:48:52 -0500

    feat: added cors bypass, fuck security

--- backend/server/server.go
+++ backend/server/server.go
@@ -9,12 +9,19 @@ const baseUrl = "/api/"
 const WeatherEndpoint = baseUrl + "weather"
 const AiEndpoint = baseUrl + "ai"
 
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func GetServerMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	// Weather Handlers here
-	mux.Handle(WeatherEndpoint, &WeatherHandler{})
+	mux.Handle(WeatherEndpoint, enableCors(&WeatherHandler{}))
 	// AI Handlers here
-	mux.Handle(SegmentEndpoint, &SegmentHandler{})
+	mux.Handle(SegmentEndpoint, enableCors(&SegmentHandler{}))
 	return mux
 }
